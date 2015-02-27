a3ab86894005f247a85dbd2486d45ad4d906b04e
Charles Munson <jetwhiz@jetwhiz.com>
2015-02-27 12:16:48 +0100

    Rely on PHP's built-in JSON encoder
    Instead of outputting JSON strings in-house, build objects and dump out with PHP JSON encoder

--- jackelow.gjye.com/api/event-id.php
+++ jackelow.gjye.com/api/event-id.php
@@ -36,89 +36,76 @@
 					WHERE `id` = ?
 			";
 			$binds = ["i", $this->REST_vars["eventID"]];
-			$res = $this->DBs->select($select, $binds);
-			
-			// No results found for this eventID
-			if ( $res->num_rows <= 0 ) {
-				echo "[]\n";
-				exit;
-			}
-			
-			echo "[\n";
-			echo "\t{\n"; 
-			$separator = '';
-			while ($row = $res->fetch_assoc()) {
-				echo
-					"$separator\t\t\"name\" : \"" . $row['name'] . "\",\n" . 
-					"\t\t\"datetimeStart\" : \"" . $row['datetimeStart'] . "\",\n" . 
-					"\t\t\"datetimeEnd\" : \"" . $row['datetimeEnd'] . "\",\n" . 
-					"\t\t\"description\" : \"" . $row['description'] . "\",\n" . 
-					"\t\t\"ownerID\" : " . $row['ownerID'] . ",\n" . 
-					"\t\t\"eventTypeID\" : " . $row['eventTypeID'] . ",\n";
-				$separator = ",\n";
-			}
-			// * // 
+			$res[1] = $this->DBs->select($select, $binds);
 			
 			
-			// Get event destinations //
-			$select = "
-					SELECT `address`, `datetimeStart`, `datetimeEnd`, `cityID`, `countryID`
-					FROM `EventDestinations` 
-					WHERE `eventID` = ?
-			";
-			$binds = ["i", $this->REST_vars["eventID"]];
-			$res = $this->DBs->select($select, $binds);
-			
-			echo "\t\t\"destinations\" : [\n";
-			$separator = '';
-			$destinations = [];
-			while ($row = $res->fetch_assoc()) {
-				if ( $this->REST_vars["simple"] == 1 ) {
-					if ( in_array($row['countryID'], $destinations) ) {
-						continue;
+			$JSON = [];
+			while ($row[1] = $res[1]->fetch_assoc()) {
+				$obj[1] = [];
+				$obj[1]["name"] = $row[1]['name'];
+				$obj[1]["datetimeStart"] = $row[1]['datetimeStart'];
+				$obj[1]["datetimeEnd"] = $row[1]['datetimeEnd'];
+				$obj[1]["description"] = $row[1]['description'];
+				$obj[1]["ownerID"] = $row[1]['ownerID'];
+				$obj[1]["eventTypeID"] = $row[1]['eventTypeID'];
+				
+				
+				// Get event destinations //
+				$select = "
+						SELECT `address`, `datetimeStart`, `datetimeEnd`, `cityID`, `countryID`
+						FROM `EventDestinations` 
+						WHERE `eventID` = ?
+				";
+				$binds = ["i", $this->REST_vars["eventID"]];
+				$res[2] = $this->DBs->select($select, $binds);
+				
+				$obj[2] = [];
+				while ($row[2] = $res[2]->fetch_assoc()) {
+					
+					if ( $this->REST_vars["simple"] == 1 ) {
+						if ( in_array($row[2]['countryID'], $obj[2]) ) {
+							continue;
+						}
+						
+						$obj[2][] = $row[2]['countryID'];
 					}
-					echo  "$separator\t\t\t" . $row['countryID'] . "";
-					$separator = ",\n";
-					$destinations[] = $row['countryID'];
+					else {
+						$obj[3] = [];
+						$obj[3]["address"] = $row[2]['address'];
+						$obj[3]["datetimeStart"] = $row[2]['datetimeStart'];
+						$obj[3]["datetimeEnd"] = $row[2]['datetimeEnd'];
+						$obj[3]["cityID"] = $row[2]['cityID'];
+						$obj[3]["countryID"] = $row[2]['countryID'];
+						
+						$obj[2][] = $obj[3];
+					}
 				}
-				else {
-					echo 
-						"$separator\t\t\t{\n" . 
-						"\t\t\t\t\"address\" : \"" . $row['address'] . "\",\n" . 
-						"\t\t\t\t\"datetimeStart\" : \"" . $row['datetimeStart'] . "\",\n" . 
-						"\t\t\t\t\"datetimeEnd\" : \"" . $row['datetimeEnd'] . "\",\n" . 
-						"\t\t\t\t\"cityID\" : " . $row['cityID'] . ",\n" . 
-						"\t\t\t\t\"countryID\" : " . $row['countryID'] . "\n" . 
-						"\t\t\t}";
-					$separator = ",\n";
+				$obj[1]["destinations"] = $obj[2];
+				// * // 
+				
+				
+				// Get event categories //
+				$select = "
+						SELECT `categoryID`
+						FROM `EventCategories` 
+						WHERE `eventID` = ?
+				";
+				$binds = ["i", $this->REST_vars["eventID"]];
+				$res[3] = $this->DBs->select($select, $binds);
+				
+				$obj[4] = [];
+				while ($row[3] = $res[3]->fetch_assoc()) {
+					$obj[4][] = $row[3]['categoryID'];
 				}
+				$obj[1]["categories"] = $obj[4];
+				// * // 
+				
+				$JSON[] = $obj[1];
 			}
-			echo "\n\t\t],\n";
 			// * // 
 			
 			
-			// Get event categories //
-			$select = "
-					SELECT `categoryID`
-					FROM `EventCategories` 
-					WHERE `eventID` = ?
-			";
-			$binds = ["i", $this->REST_vars["eventID"]];
-			$res = $this->DBs->select($select, $binds);
-			
-			echo "\t\t\"categories\" : [\n";
-			$separator = '';
-			while ($row = $res->fetch_assoc()) {
-				echo "$separator\t\t\t" . $row['categoryID'] . "";
-				$separator = ",\n";
-			}
-			echo "\n\t\t]\n";
-			// * // 
-			
-			
-			
-			echo "\t}\n";
-			echo "]\n";
+			echo json_encode($JSON, JSON_PRETTY_PRINT) . "\n\n";
 		}
 		
 	}
