860cba927a3cb33af4907fc9b2c9341142bf524c
Charles Munson <jetwhiz@jetwhiz.com>
2015-03-25 15:43:22 +0100

    Webapp: Clean up index.php page
    Break logic out into individual files to be cleaner
    send() function added for sending page to user (dies afterwards)
    Fixes issue where we would just die on invalid/no event ID

--- jackelow.gjye.com/webapp/index.php
+++ jackelow.gjye.com/webapp/index.php
@@ -18,17 +18,14 @@
 	////
 	
 	
-	// First elements should be /webapp/ 
-	if ( $queryArray[0] == "webapp" ) {
-		array_shift($queryArray);
-	}
-	//// 
-	
-	
 	// Ensure user is logged in 
 	$Auth = new Authenticate();
 	$User = $Auth->getUser();
 	$usrname = $User->getUsername();
+	if ( is_null( $User ) ) {
+		send("", "You must be logged in!", $GLOBALS["HTTP_STATUS"]["Forbidden"]);
+	}
+	////
 	
 	
 	// Send cookies along with GET requests 
@@ -40,428 +37,74 @@
 			)
 		);
 	$getContext = stream_context_create($opts);
+	////
 	
 	
-	$headTags = "";
-	$contentBodyWrapper = "";
+	// Display options for our web app 
+	$REST_types = [ 
+		"event" => "index-event.php", 
+		"createevent" => "index-createevent.php", 
+		"map" => "index-map.php"
+	];
+	////
 	
 	
 	
-	// Param indicated "map" 
-	if ( count($queryArray) && $queryArray[0] == "map" ) {
+	// First elements should be /webapp/ 
+	if ( $queryArray[0] == "webapp" ) {
 		array_shift($queryArray);
-		
-		$headTags = <<<EOHT
-
-			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD_a-dYE-RmPRjgAn1dVJfSJ9IAVvE-7rQ"></script>
-			<script type="text/javascript" src="/webapp/map.js"></script>
-
-EOHT;
-		$contentBodyWrapper = "";
 	}
+	//// 
 	
 	
-	
-	// Param indicated "createEvent" 
-	elseif ( count($queryArray) && $queryArray[0] == "createevent" ) {
-		array_shift($queryArray);
+	// Second element should be as listed above 
+	if ( array_key_exists($queryArray[0], $REST_types) ) {
+		$key = array_shift($queryArray);
+		if ( is_null($key) ){
+			send("", "Error: Request path incorrect.", $GLOBALS["HTTP_STATUS"]["Not Found"]);
+		}
 		
-		$headTags = "";
-		$contentBodyWrapper = "";
+		try {
+			require $REST_types[$key];
+		} catch (Exception $e) {
+			send("", "Error: Unknown error.", $GLOBALS["HTTP_STATUS"]["Internal Error"]);
+		}
 	}
-	
-	
-	
-	// Param indicated "event" 
-	elseif ( count($queryArray) && $queryArray[0] == "event" ) {
-		array_shift($queryArray);
-		
-		// Ensure eventID given 
-		if ( !count($queryArray) || !is_numeric($queryArray[0]) ) {
-			echo "no eventID";
-			die(0);
-		}
-		
-		
-		$eventID = $queryArray[0];
-		
-		// Pull in main event data 
-		$file = file_get_contents("https://" . $_SERVER['HTTP_HOST'] . "/api/event/" . $eventID, false, $getContext);
-		if ( !$file ) {
-			echo "can't connect to API";
-			die(0);
-		}
-		$JSON = json_decode($file, true);
-		if ( count($JSON["results"]) != 1 ){
-			echo "invalid event ID";
-			die(0);
-		}
-		
-		$eventName = $JSON["results"][0]["name"];
-		$ownerName = $JSON["results"][0]["username"];
-		$ownerID = $JSON["results"][0]["ownerID"];
-		$dateStart = date("F d, Y", strtotime($JSON["results"][0]["datetimeStart"]));
-		$datetimeEnd = date("F d, Y", strtotime($JSON["results"][0]["datetimeEnd"]));
-		$eventType = $JSON["results"][0]["eventType"];
-		
-		// Convert newlines to breaks, allow clickable links    
-		$description = Toolkit::clickify($JSON["results"][0]["description"]);
-		$description = nl2br($description);
-		////
-		
-		
-		// Pull in categories 
-		$categories = [];
-		foreach ( $JSON["results"][0]["categories"] as $category ) {
-			$categories[] = "<a class='tags-link' href='/webapp/category/" . $category["categoryID"] . "/'>" . $category["name"] . "</a>";
-		}
-		$categoryStr = implode( ", ", $categories );
-		////
-		
-		
-		// Pull in destinations 
-		$destinations = "";
-		foreach ( $JSON["results"][0]["destinations"] as $destination ) {
-			$city = $destination["cityName"];
-			$country = $destination["countryName"];
-			$ddateStart = date("F d, Y", strtotime($destination["datetimeStart"]));
-			$ddatetimeEnd = date("F d, Y", strtotime($destination["datetimeEnd"]));
-			$address = $destination["address"];
-			$img = $destination["img"];
-			$thumb = $destination["thumb"];
-			
-			$destinations .= <<<EOD
-				<li class="destination">
-					<div class="destination-thumb">
-						<a href="$img" title="$city, $country" class="lightbox-image">
-							<img src="$thumb" alt="$city, $country" title="$city, $country" class="thumb" />
-						</a>
-					</div>
-					<div class="destination-body"> 
-						<div class="dest-body-header">
-							<h3>$city, $country</h3>
-						</div>
-						<div class="dest-body-subsect">
-							$ddateStart &ndash; $ddatetimeEnd
-						</div>
-						<div class="dest-body-subsect">
-							<span class="italic">Address:</span> $address 
-						</div>
-					</div>
-					<br style="clear: both;" />
-				</li>
-
-EOD;
-		}
-		if ( $destinations != "" ) {
-			$destinations = "<li class='italic'>Destinations: </li>$destinations";
-		}
-		////
-		
-		
-		// Pull in attendants 
-		$attendants = 0;
-		$attending = "Attend";
-		$attendants_raw = file_get_contents("https://" . $_SERVER['HTTP_HOST'] . "/api/event/" . $eventID . "/attendants/", false, $getContext);
-		if ( !$attendants_raw ) {
-			echo "can't connect to API";
-			die(0);
-		}
-		$JSON_attendants = json_decode($attendants_raw, true);
-		
-		// Update # of attendants
-		$attendants = count($JSON_attendants["results"]);
-		
-		// Figure out if the current user is attending already 
-		foreach ( $JSON_attendants["results"] as $attendant ) {
-			if ( $attendant["id"] == $User->getID() ) {
-				$attending = "Unattend";
-			}
-		}
-		////
-		
-		
-		// Pull in event comments 
-		$comments = "";
-		$comments_raw = file_get_contents("https://" . $_SERVER['HTTP_HOST'] . "/api/event/" . $eventID . "/comments/", false, $getContext);
-		if ( !$comments_raw ) {
-			echo "can't connect to API";
-			die(0);
-		}
-		$JSON_comments = json_decode($comments_raw, true);
-		
-		foreach ( $JSON_comments["results"] as $commentID ) {
-			$comment_raw = file_get_contents("https://" . $_SERVER['HTTP_HOST'] . "/api/event/" . $eventID . "/comments/" . $commentID, false, $getContext );
-			if ( !$comment_raw ) {
-				echo "can't connect to API";
-				die(0);
-			}
-			$JSON_comment = json_decode($comment_raw, true);
-			
-			if ( count($JSON_comment["results"]) == 1 ) {
-				$commentUsername = $JSON_comment["results"][0]["username"];
-				
-				// Convert newlines to breaks, allow clickable links 
-				$message = Toolkit::clickify($JSON_comment["results"][0]["message"]);
-				$message = nl2br($message);
-				
-				// Convert comment timestamp to Paris timezone 
-				$tz = new DateTimeZone('Europe/Paris');
-				$dateServer = new DateTime($JSON_comment["results"][0]["datetime"]);
-				$dateServer->setTimeZone($tz);					
-				$datetime = $dateServer->format("F d, Y, H:i:s T");
-				
-				$commentDeleteLink = "";
-				if ( $JSON_comment["results"][0]["ownerID"] == $User->getID() ) {
-					$commentDeleteLink = " &mdash; <a href='javascript: void(0)' class='remove-comment' commentID='$commentID'>Delete</a>";
-				}
-				$comments .= <<<EOC
-					<li class="comment">
-						<div class="comment-header">
-							<span class="bold">$commentUsername</span> ($datetime)
-							$commentDeleteLink
-						</div>
-						<div class="comment-body">
-							$message
-						</div>
-						<br style="clear: both;" />
-					</li>
-
-EOC;
-			}
-		
-		}
-		////
-		
-		
-		
-		// Show attending / edit / delete buttons dynamically 
-		$controlLinks = "<a id='attend-event' type='$attending' href='javascript: void(0);'>$attending</a>";
-		
-		if ( $eventType == "Info" || $ownerID == $User->getID() ) {
-			$controlLinks .= " - <a id='edit-event' href='javascript: void(0);'>Edit</a>";
-		}
-		if ( $ownerID == $User->getID() ) {
-			$controlLinks .= " - <a href='javascript: void(0)' id='remove-event'>Delete</a>";
-		}
-		////
-		
-		
-		
-		$headTags = <<<EOHT
-
-			<link type="text/css" rel="stylesheet" href="/webapp/jquery/jquery.colorbox.css" />
-			<script type="text/javascript" src="/webapp/jquery/jquery.colorbox-min.js"></script>
-			
-			<link type="text/css" rel="stylesheet" href="/webapp/event.css" />
-			<script type="text/javascript" src="/webapp/event.js"></script>
-
-EOHT;
-		$contentBodyWrapper = <<<EOEP
-
-			<div id="dialog-confirm" title="Delete?">Are you sure you want to delete this?</div>
-			
-			<div id="event-block">
-				<div class="event-head event-row">
-					<div class="esubrow-left">
-						<h2>$eventName</h2>
-					</div>
-					<div class="esubrow-right">
-						<span class='bold'>Owner: </span> $ownerName<br />
-						<span class='bold'>Type: </span> $eventType<br />
-						$controlLinks
-					</div>
-					<br style="clear: both;" />
-				</div>
-				<div class="event-row">
-					<div class="esubrow-full">
-						$dateStart &ndash; $datetimeEnd
-					</div>
-				</div>
-				<div class="event-row">
-					<div class="esubrow-full">
-						There are $attendants attendants so far.
-					</div>
-				</div>
-				<div class="event-row">
-					<div class="esubrow-full">
-						$categoryStr
-					</div>
-				</div>
-				<div class="event-row">
-					<div class="description esubrow-full">
-						$description
-					</div>
-				</div>
-				<div class="event-row">
-					<ul id="destinations-block">
-						$destinations
-					</ul>
-				</div>
-				<div class="event-row">
-					<div id="comments-header">
-						Comments: 
-					</div>
-					<ul id="comments-block">
-						$comments
-						<li class="comment-add">
-							<div id="create-comment">
-								<form>
-									<textarea name="message" class="comment-add-textbox"></textarea>
-									<input type="submit" value="Comment" />
-								</form>
-							</div>
-							<br style="clear: both;" />
-						</li>
-					</ul>
-				</div>
-			</div>
-
-EOEP;
-
-	}
-	
-	
-	
-	// Otherwise it is some flavor of index 
+	// Or the main index page (list all events) 
 	else {
-		
-		
-		// Get all filters chosen to display to user 
-		$filtersObj = [];
-		while ( count($queryArray) >= 2 ) {
-			$command = array_shift($queryArray);
-			$value = array_shift($queryArray);
-			
-			if ( $command == "country" && is_numeric($value) ) {
-				$value = intval($value, 10);
-				
-				// Convert countryID to title 
-				$country_raw = file_get_contents("https://" . $_SERVER['HTTP_HOST'] . "/api/country/" . $value, false, $getContext);
-				if ( !$country_raw ) {
-					echo "can't connect to API";
-					die(0);
-				}
-				$JSON = json_decode($country_raw, true);
-				
-				if ( count($JSON["results"]) == 1 ) {
-					$filtersObj["country"] = [];
-					$filtersObj["country"]["id"] = $value;
-					$filtersObj["country"]["name"] = $JSON["results"][0]["name"];
-				}
-			}
-			elseif ( $command == "category" && is_numeric($value) ) {
-				$value = intval($value, 10);
-				
-				// Convert categoryID to title 
-				$category_raw = file_get_contents("https://" . $_SERVER['HTTP_HOST'] . "/api/category/" . $value, false, $getContext);
-				if ( !$category_raw ) {
-					echo "can't connect to API";
-					die(0);
-				}
-				$JSON = json_decode($category_raw, true);
-				
-				if ( count($JSON["results"]) == 1 ) {
-					$filtersObj["category"] = [];
-					$filtersObj["category"]["id"] = $value;
-					$filtersObj["category"]["name"] = $JSON["results"][0]["name"];
-				}
-			}
+		try {
+			require "index-all.php";
+		} catch (Exception $e) {
+			send("", "Error: Unknown error.", $GLOBALS["HTTP_STATUS"]["Internal Error"]);
 		}
-		
-		// Make chosen filters pretty to display to user 
-		$filters = [];
-		$filtersStr = "<span class='bold'>Chosen filters: </span>";
-		if ( count($filtersObj) ) {
-			
-			// If country filter was chosen 
-			if ( count($filtersObj["country"]) ) {
-				
-				// Keep any specified category filters
-				$filterKeepers = "";
-				if ( count($filtersObj["category"]) ) {
-					$filterKeepers = "category/" . $filtersObj["category"]["id"] . "/";
-				}
-				
-				$filters[] = "<a href='/webapp/" . $filterKeepers . "' class='filter'>Country: " . $filtersObj["country"]["name"] . "</a>";
-			}
-			
-			// If category filter was chosen 
-			if ( count($filtersObj["category"]) ) {
-				
-				// Keep any specified country filters
-				$filterKeepers = "";
-				if ( count($filtersObj["country"]) ) {
-					$filterKeepers = "country/" . $filtersObj["country"]["id"] . "/";
-				}
-				
-				$filters[] = "<a href='/webapp/" . $filterKeepers . "' class='filter'>Category: " . $filtersObj["category"]["name"] . "</a>";
-			}
-			
-			$filtersStr .= implode( ", ", $filters );
-		}
-		else {
-			$filtersStr .= "<span class='italic'>None</span>";
-		}
-		
-		
-		$headTags = <<<EOHT
-
-				<link type="text/css" rel="stylesheet" href="/webapp/index.css" />
-				<script type="text/javascript" src="/webapp/index.js"></script>
-
-EOHT;
-
-		$contentBodyWrapper = <<<EOBW
-
-			<h2>&nbsp;Welcome, $usrname!</h2>
-			<div id="filters">$filtersStr</div>
-			
-			<ul id="sticky-injection-point"></ul>
-			<a href="javascript: void(0);" id="load-infos" title="Load informational events">Load informational events</a>
-			
-			<ul id="premium-injection-point"></ul>
-			
-			<ul id="injection-point"></ul>
-			<div class="endOfResults">End of results</div>
-			
-			<div id="event-template">
-				<li class="event-block">
-					<div class="tr">
-						<div class="thumb-cell"></div>
-						<div class="info-cell">
-							<div class="title-cell"></div>
-							<div class="username-cell"></div>
-							<div class="dates-cell"></div>
-							<div class="tags-cell"></div>
-							<div class="description-cell"></div>
-							<br style="clear: both;" />
-						</div>
-						<br style="clear: both;" />
-					</div>
-					<br style="clear: both;" />
-				</li>
-			</div>
-
-EOBW;
 	}
+	////
 	
 	
-	
-	
-	// Load template html 
-	$template = file_get_contents("template.html");
-	if ( !$template ) {
-		echo "can't get template";
-		die(0);
+	// Sends template page to the user, including required headers, body and return code 
+	function send( $header, $body, $code ) {
+		
+		// Load template html 
+		$template = file_get_contents("template.html");
+		if ( !$template ) {
+			$code = $GLOBALS["HTTP_STATUS"]["Internal Error"];
+			$body = "Error: Can't get template";
+		}
+		
+		// Print out header status 
+		http_response_code($code);
+		
+		// Replace head and body in template 
+		$template_pop = str_replace("#headTags#", $header, $template);
+		$template_pop = str_replace("#contentBodyWrapper#", $body, $template_pop);
+		
+		// Declare we're returning HTML in UTF-8
+		header('Content-Type: text/html; charset=utf-8');
+		
+		// Send to user and quit 
+		echo $template_pop;
+		die;
 	}
+	////
 	
-	$template_pop = str_replace("#headTags#", $headTags, $template);
-	$template_pop = str_replace("#contentBodyWrapper#", $contentBodyWrapper, $template_pop);
-	
-	
-	// Declare we're returning HTML in UTF-8
-	header('Content-Type: text/html; charset=utf-8');
-	
-	echo $template_pop;
 ?>
